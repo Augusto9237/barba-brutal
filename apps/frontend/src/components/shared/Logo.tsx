662ec2fc08f4779c927ee0e4e4c5ae717268b568
Augusto9237 <augusto.souza8330@gmail.com>
2024-08-09 01:27:38 -0300

    feat: update logo

--- apps/frontend/src/components/shared/Logo.tsx
+++ apps/frontend/src/components/shared/Logo.tsx
@@ -7,7 +7,7 @@ export default function Logo() {
             <Image src="/logo.png" alt="Logo" width={65} height={65} className="hidden sm:block" />
             <Image src="/logo.png" alt="Logo" width={50} height={50} className="block sm:hidden" />
             <div className="flex flex-col justify-center h-full">
-                <span className="text-base sm:text-2xl font-extralight leading-6 tracking-widest text-gradient">
+                <span className="max-sm:text-sm sm:text-base font-extralight leading-6 tracking-widest text-gradient">
                     Barber
                 </span>
                 <span className="text-base sm:text-[24px] font-bold leading-6 pl-px text-gradient">
